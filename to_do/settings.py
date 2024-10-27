0ffb74283a994e82959f2288cb6ba24b14ffd189
Filipe Abner <filipeabnersantos@gmail.com>
2024-10-27 17:04:30 -0300

    Atualizar configurações do banco de dados para usar o driver MySQL correto

--- to_do/settings.py
+++ to_do/settings.py
@@ -81,12 +81,12 @@ WSGI_APPLICATION = 'to_do.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'mysql.connector.django',
-        'NAME':config('DB_NAME'),
-        'USER':config('DB_USER'),
-        'PASSWORD':config('DB_PASSWORD'),
-        'HOST':config('DB_HOST'),
-        'PORT':config('DB_PORT'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
